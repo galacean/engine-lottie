1497769924f4f62cde6e34d96cab2a70fd9f0d73
eyworldwide <eyworldwide@foxmail.com>
2021-09-07 19:48:41 +0800

    fix: child layer did not update bugfix

--- src/LottieAnimation.ts
+++ src/LottieAnimation.ts
@@ -89,9 +89,9 @@ export class LottieAnimation extends Script {
 
 				if (layer.parent) {
 					children.push(layer);
-				} else {
-					comp.addChild(element);
 				}
+
+				comp.addChild(element);
 			}
 		}
 
