5429d0fb43c003b0f78cb000899205376ace066b
eyworldwide <eyworldwide@foxmail.com>
2021-09-29 13:04:26 +0800

    fix: clone

--- src/core/element/SpriteLottieElement.ts
+++ src/core/element/SpriteLottieElement.ts
@@ -14,13 +14,21 @@ export default class SpriteLottieLayer extends BaseLottieElement {
     if (layer.refId) {
       if (childEntity) {
         this.entity = childEntity;
+
         const spriteRenderer = childEntity.getComponent(SpriteRenderer);
+
+        this.spriteRenderer = spriteRenderer;
+        this.sprite = spriteRenderer.sprite;
+
+        const { atlasRegion, texture } = this.sprite;
+
         // @ts-ignore
         spriteRenderer._renderSortId = this.index;
         // @ts-ignore
         spriteRenderer._customRootEntity = entity;
-        this.spriteRenderer = spriteRenderer;
-        this.sprite = spriteRenderer.sprite;
+
+        this.width = atlasRegion.width * texture.width;
+        this.height = atlasRegion.height * texture.height;
       }
       else {
         this.sprite = atlas.getSprite(layer.refId);
@@ -28,11 +36,13 @@ export default class SpriteLottieLayer extends BaseLottieElement {
 
         const spriteEntity = new Entity(entity.engine, layer.nm);
         const spriteRenderer = spriteEntity.addComponent(SpriteRenderer);
+
         spriteRenderer.sprite = this.sprite;
         // @ts-ignore
         spriteRenderer._renderSortId = this.index;
         // @ts-ignore
         spriteRenderer._customRootEntity = entity;
+
         this.entity = spriteEntity;
         this.spriteRenderer = spriteRenderer;
 
