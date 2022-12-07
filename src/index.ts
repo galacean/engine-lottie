921a766ebbdbe1ad76b26467e940255ab70a3f09
singlecoder <singlecoder2014@gmail.com>
2022-12-07 19:13:22 +0800

    opt loader register

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
+import { Loader } from 'oasis-engine';
+import { LottieAnimation } from './LottieAnimation';
+
 export { LottieAnimation } from './LottieAnimation';
 export { LottieLoader } from "./LottieLoader";
 export { EditorLottieLoader } from "./EditorLottieLoader";
 export { LottieResource } from "./LottieResource";
+
+Loader.registerClass("LottieAnimation", LottieAnimation);
