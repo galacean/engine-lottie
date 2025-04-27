6c5aa0a763c437538d4016ccfdb99ecc1ca80ac6
singlecoder <singlecoder2014@gmail.com>
2025-04-27 21:40:16 +0800

    opt code

--- src/EditorLottieLoader.ts
+++ src/EditorLottieLoader.ts
@@ -10,13 +10,14 @@ export class EditorLottieLoader extends Loader<LottieResource> {
   // @ts-ignore
   load(item: LoadItem, resourceManager: ResourceManager): AssetPromise<LottieResource> {
     return new AssetPromise((resolve) => {
+      const { url } = item;
       // @ts-ignore
-      resourceManager._request<any>(item.url, { type: "json" }).then((data) => {
+      resourceManager._request<any>(url, { type: "json" }).then((data) => {
         const { jsonUrl, atlasUrl } = data;
         // @ts-ignore
-        const jsonPromise = resourceManager._request(Utils.resolveAbsoluteUrl(item.url, jsonUrl), resourceManager);
+        const jsonPromise = resourceManager._request(Utils.resolveAbsoluteUrl(url, jsonUrl), resourceManager);
         const atlasPromise = resourceManager.load({
-          url: Utils.resolveAbsoluteUrl(item.url, atlasUrl),
+          url: Utils.resolveAbsoluteUrl(url, atlasUrl),
           type: AssetType.SpriteAtlas
         });
 
